790e07062bf2e8a61fbc208242eaba31b4425e6f
Marcus Harbol <mharbol@users.noreply.github.com>
2023-11-30 17:02:23 -0500

    day 1 setup

---
+++ solution/days/day01.go
@@ -0,0 +1,11 @@
+package days
+
+func Day01Part1(lines []string) (string, error) {
+
+	return "", nil
+}
+
+func Day01Part2(lines []string) (string, error) {
+
+	return "", nil
+}
--- solution/solution_registry.go
+++ solution/solution_registry.go
@@ -4,4 +4,5 @@ import "github.com/mharbol/aoc-2023/solution/days"
 
 var allSolutions = map[uint8]*solFuncTuple{
     0: {days.Day00Part1, days.Day00Part2},
+    1: {days.Day01Part1, days.Day01Part2},
 }
