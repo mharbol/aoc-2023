fcad6dc0b2cfb992a24b657f976887a2ac4d53d0
Marcus Harbol <mharbol@users.noreply.github.com>
2023-12-02 06:50:36 -0500

    day 2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Advent of Code - 2023
-Doing the [Advent of Code](https://adventofcode.com/) [this year](https://adventofcode.com/2023/) in [Go\[lang\]](https://go.dev/).
+Doing the [Advent of Code](https://adventofcode.com/) for [2023](https://adventofcode.com/2023/) in [Go\[lang\]](https://go.dev/).
 Probably none of these solutions are optimal... FYI.
 
 I went a little overboard with the interfaces and methods; I'm coming from the Java world and can't shake it just yet.
@@ -14,3 +14,8 @@ type... but they didn't include it in the stdlib so I can't or have to make my o
 Nothing too fancy.
 For part one, I just looped forward and back to find the first and last digits.
 For part two, I decided not to get fancy with front and back sliders and just made an array with all the "numbers" found in the string.
+
+### [Day 02](solution/days/day02.go)
+A little hacky just copying logic from part one to part two... but it works.
+Systematically broke down the lines using `strings.Split()` until I got a count and color.
+From there just did some easy sums and products to get the desired results.
--- solution/days/day02.go
+++ solution/days/day02.go
@@ -1,11 +1,129 @@
 package days
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func Day02Part1(lines []string) (string, error) {
 
-	return "", nil
+	acc := 0
+
+	const (
+		RED_MAX   int = 12
+		GREEN_MAX int = 13
+		BLUE_MAX  int = 14
+	)
+
+	for index, line := range lines {
+
+		gameNum := index + 1
+
+		// prune off game
+		var start int
+		if gameNum < 10 {
+			start = 8
+		} else if gameNum < 100 {
+			start = 9
+		} else {
+			start = 10
+		}
+		game := line[start:]
+
+		draws := strings.Split(game, "; ")
+
+		isPossible := true
+
+		for _, draw := range draws {
+			pairs := strings.Split(draw, ", ")
+
+			for _, pairStr := range pairs {
+				var pair []string = strings.Split(pairStr, " ")
+				count, _ := strconv.Atoi(pair[0])
+				cubeColor := pair[1]
+				switch cubeColor {
+				case "red":
+					if count > RED_MAX {
+						isPossible = false
+					}
+					break
+				case "green":
+					if count > GREEN_MAX {
+						isPossible = false
+					}
+					break
+				case "blue":
+					if count > BLUE_MAX {
+						isPossible = false
+					}
+					break
+				}
+			}
+		}
+		if isPossible {
+			acc += gameNum
+		}
+	}
+
+	return fmt.Sprint(acc), nil
 }
 
 func Day02Part2(lines []string) (string, error) {
 
-	return "", nil
+	acc := 0
+
+	for index, line := range lines {
+
+		gameNum := index + 1
+
+		// prune off game
+		var start int
+		if gameNum < 10 {
+			start = 8
+		} else if gameNum < 100 {
+			start = 9
+		} else {
+			start = 10
+		}
+		game := line[start:]
+
+		minRed := 0
+		minBlue := 0
+		minGreen := 0
+
+		draws := strings.Split(game, "; ")
+
+		for _, draw := range draws {
+			pairs := strings.Split(draw, ", ")
+
+			for _, pairStr := range pairs {
+				var pair []string = strings.Split(pairStr, " ")
+				count, _ := strconv.Atoi(pair[0])
+				cubeColor := pair[1]
+
+				switch cubeColor {
+				case "red":
+					if count > minRed {
+						minRed = count
+					}
+					break
+				case "green":
+					if count > minGreen {
+						minGreen = count
+					}
+					break
+				case "blue":
+					if count > minBlue {
+						minBlue = count
+					}
+					break
+				}
+			}
+		}
+		power := minBlue * minRed * minGreen
+		acc += power
+	}
+
+	return fmt.Sprint(acc), nil
 }
