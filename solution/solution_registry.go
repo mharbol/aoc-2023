583b1f2c452502693d543534028debc727124659
Marcus Harbol <mharbol@users.noreply.github.com>
2023-12-04 22:57:30 -0500

    format solution registry

--- solution/solution_registry.go
+++ solution/solution_registry.go
@@ -3,10 +3,10 @@ package solution
 import "github.com/mharbol/aoc-2023/solution/days"
 
 var allSolutions = map[uint8]*solFuncTuple{
-    0: {days.Day00Part1, days.Day00Part2},
-    1: {days.Day01Part1, days.Day01Part2},
-    2: {days.Day02Part1, days.Day02Part2},
-    3: {days.Day03Part1, days.Day03Part2},
-    4: {days.Day04Part1, days.Day04Part2},
-    5: {days.Day05Part1, days.Day05Part2},
+	0: {days.Day00Part1, days.Day00Part2},
+	1: {days.Day01Part1, days.Day01Part2},
+	2: {days.Day02Part1, days.Day02Part2},
+	3: {days.Day03Part1, days.Day03Part2},
+	4: {days.Day04Part1, days.Day04Part2},
+	5: {days.Day05Part1, days.Day05Part2},
 }
