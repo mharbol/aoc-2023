6677af2dcae24d5d4684e7cc2ed755bdb952b534
Marcus Harbol <marcusharbol@gmail.com>
2023-12-08 07:47:11 -0500

    day 8 part 1

--- solution/days/day08.go
+++ solution/days/day08.go
@@ -1,11 +1,50 @@
 package days
 
+import "fmt"
+
 func Day08Part1(lines []string) (string, error) {
 
-	return "", nil
+	instructions := lines[0]
+	length := len(instructions)
+	digraph := makeDigraph(lines[2:])
+	currNode := "AAA"
+	count := 0
+
+	for currNode != "ZZZ" {
+		instruction := instructions[count%length]
+		count++
+		if instruction == 'L' {
+			currNode = digraph[currNode].left
+		} else {
+			currNode = digraph[currNode].right
+		}
+	}
+
+	return fmt.Sprint(count), nil
 }
 
 func Day08Part2(lines []string) (string, error) {
 
 	return "", nil
+}
+
+type lrPair struct {
+	left  string
+	right string
+}
+
+func makeDigraph(lines []string) map[string]lrPair {
+
+	digraph := make(map[string]lrPair)
+
+	for _, line := range lines {
+		key := line[:3]
+		pair := lrPair{
+			left:  line[7:10],
+			right: line[12:15],
+		}
+		digraph[key] = pair
+	}
+
+	return digraph
 }
