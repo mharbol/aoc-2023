47a8b13dfd6c762ee043098d3f4b5636af465b13
Marcus Harbol <mharbol@users.noreply.github.com>
2023-12-11 20:44:58 -0500

    day 10 and 11 part 1

--- solution/days/day10.go
+++ solution/days/day10.go
@@ -1,11 +1,110 @@
 package days
 
+import "fmt"
+
 func Day10Part1(lines []string) (string, error) {
 
-	return "", nil
+	s := findS(lines)
+	fmt.Println(s.row, s.col)
+
+    prev := s
+    curr := coord{row: prev.row + 1, col: prev.col}
+
+    length := 1
+    for lines[curr.row][curr.col] != 'S' {
+        fmt.Println("curr: ", curr, " prev: ", prev)
+        temp := curr
+        curr = takeStep(curr, prev, lines)
+        prev = temp
+        length++
+    }
+
+	return fmt.Sprint(length / 2 + length % 2), nil
 }
 
 func Day10Part2(lines []string) (string, error) {
 
 	return "", nil
+}
+
+type coord struct {
+	row int
+	col int
+}
+
+func findS(lines []string) coord {
+	for row := 0; row < len(lines); row++ {
+		for col := 0; col < len(lines[row]); col++ {
+			if lines[row][col] == 'S' {
+				return coord{row: row, col: col}
+			}
+		}
+	}
+	return coord{-1, -1}
+}
+
+func takeStep(currPos, prevPos coord, lines []string) coord {
+
+	switch lines[currPos.row][currPos.col] {
+	case '|':
+		if currPos.row > prevPos.row { // go south
+			return goSouth(currPos)
+		} else { // go north
+			return goNorth(currPos)
+		}
+	case '-':
+		if currPos.col > prevPos.col { // go east
+			return goEast(currPos)
+		} else { // go west
+			return goWest(currPos)
+		}
+	case 'L':
+		if currPos.row > prevPos.row { // go east
+			return goEast(currPos)
+		} else { // go north
+			return goNorth(currPos)
+		}
+	case 'J':
+		if currPos.row > prevPos.row { // go west
+			return goWest(currPos)
+		} else {
+			return goNorth(currPos)
+		}
+    case '7':
+        if currPos.col > prevPos.col {
+            return goSouth(currPos)
+        } else {
+            return goWest(currPos)
+        }
+    case 'F':
+        if currPos.row < prevPos.row {
+            return goEast(currPos)
+        } else {
+            return goSouth(currPos)
+        }
+    default:
+        fmt.Println("Came to a ", lines[currPos.row][currPos.col], " at ", currPos)
+	}
+
+	return coord{-1, -1}
+}
+
+func goNorth(currPos coord) coord {
+    fmt.Println("go north")
+	return coord{row: currPos.row - 1, col: currPos.col}
+}
+
+func goSouth(currPos coord) coord {
+    fmt.Println("go south")
+	return coord{row: currPos.row + 1, col: currPos.col}
+}
+
+func goEast(currPos coord) coord {
+    fmt.Println("go east")
+	return coord{row: currPos.row, col: currPos.col + 1}
+}
+
+func goWest(currPos coord) coord {
+    fmt.Println("go west")
+	return coord{row: currPos.row, col: currPos.col - 1}
 }
--- solution/days/day11.go
+++ solution/days/day11.go
@@ -1,11 +1,91 @@
 package days
 
+import "fmt"
+
 func Day11Part1(lines []string) (string, error) {
 
-	return "", nil
+	acc := 0
+
+	universe := expandUniverse(lines)
+	galxies := getGalaxies(universe)
+
+	for x := 0; x < len(galxies)-1; x++ {
+		for y := x + 1; y < len(galxies); y++ {
+			acc += manhattanDistance(galxies[x], galxies[y])
+		}
+	}
+
+	return fmt.Sprint(acc), nil
 }
 
 func Day11Part2(lines []string) (string, error) {
 
 	return "", nil
+}
+
+type galaxyCoord struct {
+	row int
+	col int
+}
+
+func i32Abs(n int32) int32 {
+	var y int32 = (n >> 31)
+	return (n ^ y) - y
+}
+
+func expandUniverse(lines []string) [][]byte {
+
+	// get expanded columns
+	doubleColSet := make(map[int]bool)
+	for col := 0; col < len(lines[0]); col++ {
+		allDots := true
+		for row := 0; row < len(lines); row++ {
+			if lines[row][col] == '#' {
+				allDots = false
+				break
+			}
+		}
+		if allDots {
+			doubleColSet[col] = true
+		}
+	}
+
+	// loop over and make rows
+	var out [][]byte
+	for row := 0; row < len(lines); row++ {
+		var line []byte
+		allDots := true
+		for col := 0; col < len(lines[row]); col++ {
+			_, ok := doubleColSet[col]
+			if ok {
+				line = append(line, '.')
+			}
+			if lines[row][col] != '.' {
+				allDots = false
+			}
+			line = append(line, lines[row][col])
+		}
+		out = append(out, line)
+		if allDots {
+			out = append(out, line)
+		}
+	}
+
+	return out
+}
+
+func getGalaxies(universe [][]byte) []galaxyCoord {
+	var out []galaxyCoord
+	for row := 0; row < len(universe); row++ {
+		for col := 0; col < len(universe[row]); col++ {
+			if universe[row][col] == '#' {
+				out = append(out, galaxyCoord{row: row, col: col})
+			}
+		}
+	}
+	return out
+}
+
+func manhattanDistance(g1, g2 galaxyCoord) int {
+	return int(i32Abs(int32(g1.col)-int32(g2.col))) + int(i32Abs(int32(g1.row)-int32(g2.row)))
 }
