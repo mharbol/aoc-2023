d20df315a21431323ca961fb42311db094a176af
Marcus Harbol <mharbol@users.noreply.github.com>
2024-02-26 23:03:10 -0500

    refactor day 5

--- solution/days/day05.go
+++ solution/days/day05.go
@@ -2,14 +2,13 @@ package days
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 )
 
 func Day05Part1(lines []string) (string, error) {
 
-	var vals []int
+	minVal := 1<<31 - 1 // max int32 val
 
 	seeds, mappers := parsePart1(lines)
 
@@ -18,12 +17,13 @@ func Day05Part1(lines []string) (string, error) {
 		for _, mapper := range mappers {
 			val = mapper.getDesination(val)
 		}
-		vals = append(vals, val)
+		// determine new minVal
+		if minVal > val {
+			minVal = val
+		}
 	}
 
-	sort.Ints(vals)
-
-	return fmt.Sprint(vals[0]), nil
+	return fmt.Sprint(minVal), nil
 }
 
 func Day05Part2(lines []string) (string, error) {
@@ -31,23 +31,11 @@ func Day05Part2(lines []string) (string, error) {
 	return "10834440", nil
 }
 
+// basic struct to determine if an `int` is in a certain span and map to an output value
 type spanChecker struct {
 	sourceStart int
 	sourceEnd   int
 	outputStart int
-}
-
-// checks to see if the given source is inside the range for this
-// spanChecker's definition
-func (sc *spanChecker) isInRange(source int) bool {
-	return source >= sc.sourceStart && source < sc.sourceEnd // strictly less than because the span starts on sourceStart
-}
-
-// returns the output for the given input.
-// This assumes that the input is in the defined range
-func (sc *spanChecker) getOutput(input int) int {
-	delta := input - sc.sourceStart
-	return sc.outputStart + delta
 }
 
 func newSpanChecker(sourceStart, outputStart, givenSpan int) *spanChecker {
@@ -58,7 +46,7 @@ func newSpanChecker(sourceStart, outputStart, givenSpan int) *spanChecker {
 	}
 }
 
-func makeSpanCheckerFromString(str string) *spanChecker {
+func newSpanCheckerFromString(str string) *spanChecker {
 	numsStrSlc := strings.Split(str, " ")
 	desStart, _ := strconv.Atoi(numsStrSlc[0])
 	srcStart, _ := strconv.Atoi(numsStrSlc[1])
@@ -66,44 +54,54 @@ func makeSpanCheckerFromString(str string) *spanChecker {
 	return newSpanChecker(srcStart, desStart, initSpan)
 }
 
-type resourceMapper struct {
-	spanCheckers []*spanChecker
+// checks to see if the given source INT!!! is inside the span for this
+// spanChecker's definition
+func (sc *spanChecker) isIntInSpan(source int) bool {
+	return source >= sc.sourceStart && source < sc.sourceEnd // strictly less than because the span starts on sourceStart
 }
 
-func (rm *resourceMapper) getDesination(input int) int {
-	// loop throught the spanCheckers to see if any are in range
-	// return the output if so
-	for _, checker := range rm.spanCheckers {
-		if checker.isInRange(input) {
-			return checker.getOutput(input)
-		}
-	}
-	// if none found, use the input number as the destination
-	return input
+// returns the output for the given input.
+// This assumes that the input is in the defined span
+func (sc *spanChecker) getSpannedOutput(input int) int {
+	delta := input - sc.sourceStart
+	return sc.outputStart + delta
+}
+
+// compares spanCheckers for the purpose of sorting
+// sorting criteria is value of sourceStart
+func (sc *spanChecker) compareTo(other *spanChecker) int {
+	return sc.sourceStart - other.sourceStart
+}
+
+// aggregate of spanCheckers to determine the destination of any given input value
+type resourceMapper struct {
+	spanCheckers []*spanChecker
 }
 
 func newResourceMapper(strings []string) *resourceMapper {
 	var checkers []*spanChecker
 
 	for _, checkerStr := range strings {
-		checkers = append(checkers, makeSpanCheckerFromString(checkerStr))
+		checkers = append(checkers, newSpanCheckerFromString(checkerStr))
 	}
 
 	return &resourceMapper{spanCheckers: checkers}
 }
 
-func parseSeeds(line string) []int {
-	var ret []int
-	seedsStrArr := strings.Split(line[7:], " ")
-	for _, numStr := range seedsStrArr {
-		num, _ := strconv.Atoi(numStr)
-		ret = append(ret, num)
+func (rm *resourceMapper) getDesination(input int) int {
+	for _, checker := range rm.spanCheckers {
+		if checker.isIntInSpan(input) {
+			return checker.getSpannedOutput(input)
+		}
 	}
-	return ret
+	// if none found, use the input number as the destination
+	return input
 }
 
+// Functions for parsing input to this problem
+
 func parsePart1(lines []string) ([]int, []*resourceMapper) {
-	seeds := parseSeeds(lines[0])
+	seeds := parseSeedsPart1(lines[0])
 	entries := getBlocksOfEntries(lines)
 	var mappers []*resourceMapper
 	for _, entry := range entries {
@@ -111,6 +109,16 @@ func parsePart1(lines []string) ([]int, []*resourceMapper) {
 	}
 
 	return seeds, mappers
+}
+
+func parseSeedsPart1(line string) []int {
+	var ret []int
+	seedsStrArr := strings.Split(line[7:], " ")
+	for _, numStr := range seedsStrArr {
+		num, _ := strconv.Atoi(numStr)
+		ret = append(ret, num)
+	}
+	return ret
 }
 
 func getBlocksOfEntries(lines []string) [][]string {
