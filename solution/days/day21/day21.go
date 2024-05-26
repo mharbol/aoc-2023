8405db95ce8473fb39494f087e89f32df4546dd6
Marcus Harbol <mharbol@users.noreply.github.com>
2024-05-25 23:09:05 -0600

    day 21 part 1

--- solution/days/day21/day21.go
+++ solution/days/day21/day21.go
@@ -1,8 +1,31 @@
 package day21
 
+import "fmt"
+
 func Part1(lines []string) string {
 
-	return ""
+	const MAX_STEP int = 64
+
+	walkMap := make(map[coord]int)
+
+	walkMap[findStart(lines)] = 0
+
+	for lastStep := 0; lastStep < MAX_STEP; lastStep++ {
+		for coordinate, count := range walkMap {
+			if count == lastStep {
+				walkFromPoint(coordinate, lines, walkMap, lastStep+1)
+			}
+		}
+	}
+
+	total := 0
+	for _, count := range walkMap {
+		if count%2 == 0 {
+			total++
+		}
+	}
+
+	return fmt.Sprint(total)
 }
 
 func Part2(lines []string) string {
